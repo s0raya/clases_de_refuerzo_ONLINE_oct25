ae853ffaad1261610781005f527850e3400a795f
s0raya <soraya.alcobet@gmail.com>
2025-10-29 20:05:27 +0100

    Realizado

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburgerIcon.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle("active");
+})
