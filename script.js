3b0fc6e80375657ec9fd47c871c101ce0d6a3deb
s0raya <soraya.alcobet@gmail.com>
2025-11-04 20:39:40 +0100

    desplegables menu

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const hamburgerIcon = document.getElementById("hamburger-icon");
+const menuToggle = document.getElementById("hamburger-icon");
 const navbar = document.getElementById("navbar");
+const submenuLinks = document.querySelectorAll('.has-submenu > a');
 
-hamburgerIcon.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
     navbar.classList.toggle("active");
 })
+
+submenuLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        link.parentElement.classList.toggle('active')
+    })
+})
+
+/*********** Para cerrar otros submenús al abrir uno *******/
+
+/*submenuLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        submenuLinks.forEach(l => {
+            if (l !== link) l.parentElement.classList.remove('active');
+        });
+        link.parentElement.classList.toggle('active');
+    });
+});*/
