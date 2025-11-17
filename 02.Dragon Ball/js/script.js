d54cf9a33e2429b1b40163b37f26ad924009e6c5
s0raya <soraya.alcobet@gmail.com>
2025-11-17 20:26:31 +0100

    02 terminada, separado en dos paginas

--- 02.Dragon Ball/js/script.js
+++ 02.Dragon Ball/js/script.js
@@ -1,18 +1,17 @@
 const charactersContainer = document.getElementById("characters");
 const favoritesContainer = document.getElementById("favorites");
-const loadBtn = document.getElementById("loadBtn");
 const searchInput = document.getElementById("searchInput");
 const btn_prev = document.getElementById("btn_prev");
 const btn_next = document.getElementById("btn_next");
 
-const url = "https://dragonball-api.com/api/characters";
+let currentUrl = "https://dragonball-api.com/api/characters";
 
 let characters = [];
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-let page = 1;
 
-const loadCharacters = (page) => {
-    fetch(`${url}/?page=${page}&limit=8`)
+const loadCharacters = (url) => {
+    console.log(url)
+    fetch(`${url}`)
     .then(response => {
         if(!response.ok) {
             throw new Error('La consulta no se ha podido realizar')
@@ -20,21 +19,32 @@ const loadCharacters = (page) => {
         return response.json();
     })
     .then(data => {
-        const { items } = data;
-        characters = items;
-        renderCharacters(characters)
+        characters = data.items || data;
+
+        /* si no pongo esto, al buscar un personaje links no existe y da error) */
+        if(data.items && data.links) {
+            updatePagination(data.links.next, data.links.previous)
+        }
+        if(charactersContainer) renderCharFavs(characters, charactersContainer)
+        if(favoritesContainer) renderCharFavs(favorites, favoritesContainer)
     }) 
     .catch (err => console.log("Error al cargar: ", err))
 }
 
-loadCharacters(page);
+loadCharacters(currentUrl);
 
-const renderCharacters = (characters) => {
-    charactersContainer.innerHTML = '';
-    characters.forEach(item => {
-        const { id, name, ki, maxKi, race, gender, affiliation, image } = item;
+const renderCharFavs = (charactersList, container) => {
+    container.innerHTML = '';
+    if(!charactersList) return;
+
+
+    charactersList.forEach(character => {
+        const { id, name, ki, maxKi, race, gender, affiliation, image } = character;
         const card = document.createElement("div");
         card.className = "card";
+        if(favorites.some(fav => fav.id === character.id)) {
+            card.classList.add('card_favorite');
+        }
         card.innerHTML = `
             <div class="container_img">
                 <img src="${image}" alt="${name}" class="img_character">
@@ -50,70 +60,67 @@ const renderCharacters = (characters) => {
                 <span class="info_orange">${affiliation}</span>
             </div>
         `;
-        card.addEventListener("click", () => addFavorite(id));
-        charactersContainer.appendChild(card);
+        card.addEventListener('click', () => {
+            card.classList.toggle('card_favorite');
+            toggleFavorite(id);
+        })
+        container.appendChild(card);
     });
-    renderFavorites();
 }
 
-const addFavorite = (id) => {
-    const found = characters.find(char => char.id === id);
-    if(!favorites.find(f => f.id === id)) {
-        favorites = [...favorites, found];
-        localStorage.setItem("favorites", JSON.stringify(favorites));
-        renderFavorites();
+const toggleFavorite = (id) => {
+    const exists = favorites.some(f => f.id === id);
+
+    if(exists) {
+        favorites = [...favorites.filter(f => f.id !== id)]; 
+        /*Se podría usar sin el spread operator, pero lo usamos para explicar que así creamos un nuevo array, 
+        no modificamos el existente, esto es muy importante para React, estados, inmutabilidad..etc */
+    } else {
+        const found = characters.find(c => c.id === id);
+        favorites = [...favorites, found] 
+        /* Se podría usar directamente un push, pero la idea es usar el spread operator para que lo vean, así
+        copiamos el array actual, añadimos el nuevo personaje al final y creamos un array nuevo */
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+
+    if(favoritesContainer) {
+        renderCharFavs(favorites, favoritesContainer)
     }
 }
 
-const renderFavorites = () => {
-    favoritesContainer.innerHTML = '';
-    favorites.forEach(fav => {
-        const { id, name, ki, maxKi, race, gender, affiliation, image} = fav;
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-            <div class="container_img">
-                <img src="${image}" alt="${name}" class="img_character">
-            </div>
-            <div class="container_info">
-                <h3>${name}</h3>
-                <span class="info_orange">${race} - ${gender}</span>
-                <span class="info_white">Base KI:</span>
-                <span class="info_orange">${ki}</span>
-                <span class="info_white">Total KI:</span>
-                <span class="info_orange">${maxKi}</span>
-                <span class="info_white">Afilliation</span>
-                <span class="info_orange">${affiliation}</span>
-            </div>
-        `;
-        card.addEventListener("click", () => removeFavorite(id));
-        favoritesContainer.appendChild(card);
+// Buscador en Home
+if(searchInput && charactersContainer) {
+    searchInput.addEventListener('input', (e) => {
+        const term = e.target.value.trim().toLowerCase();
+        if(term === "") {
+            loadCharacters(currentUrl);
+        } else {
+            loadCharacters(`https://dragonball-api.com/api/characters?name=${term}`)
+        }
     })
 }
 
-const removeFavorite = (id) => {
-    favorites = favorites.filter(f => f.id !== id);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    renderFavorites();
+// Buscador en favoritos
+if(searchInput && favoritesContainer) {
+    searchInput.addEventListener('input', (e) => {
+        const term = e.target.value.trim().toLowerCase();
+        const filtered = favorites.filter(f => f.name.toLowerCase().includes(term));
+        renderCharFavs(filtered, favoritesContainer)
+    });
 }
 
-searchInput.addEventListener("input", (e) => {
-    const term = e.target.value.toLowerCase();
-    const filtered = characters.filter(c => c.name.toLowerCase().includes(term));
-    renderCharacters(filtered);
-})
+const updatePagination = (next, prev) => {
+    btn_next.dataset.url = next
+    btn_prev.dataset.url = prev
+}
 
 btn_next.addEventListener('click', () => {
-    if(page <= 7) {
-        page++;
-        loadCharacters(page);
-    }
+    const nextUrl = btn_next.dataset.url;
+    if(nextUrl) loadCharacters(nextUrl)
 })
 
 btn_prev.addEventListener('click', () => {
-    if(page > 1) {
-        page--;
-        loadCharacters(page);
-    }
+    const prevUrl = btn_prev.dataset.url;
+    if(prevUrl) loadCharacters(prevUrl)
 })
-
